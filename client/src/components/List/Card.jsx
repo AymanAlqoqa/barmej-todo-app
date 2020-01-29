870be60bab71586671531965b76b408bc6477c26
AymanAlqoqa <aalqouqa@gmail.com>
2020-01-29 12:06:03 +0200

    use callback fun instead of binding this

--- client/src/components/List/Card.jsx
+++ client/src/components/List/Card.jsx
@@ -5,7 +5,7 @@ export default ({ title, description, deleteCard, id }) => {
     <div className="card">
       <h3>{title}</h3>
       <p>{description}</p>
-      <span className="card-delete" onClick={deleteCard.bind(this, id)}>
+      <span className="card-delete" onClick={() => deleteCard(id)}>
         <i className="material-icons">delete</i>
       </span>
     </div>
